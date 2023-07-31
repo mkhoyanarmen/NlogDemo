7d5063b989b393a70b0b054e7cc6ca4365658ce2
mkhoyanarmen <93216230+mkhoyanarmen@users.noreply.github.com>
2023-07-31 14:28:57 +0400

    Add project files.

---
+++ NlogDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NlogDemo
+{
+    internal class Program
+    {
+        static Logger logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 0;
+                int b = 16;
+                int c = b / a;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
